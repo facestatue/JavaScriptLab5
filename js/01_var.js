8308926c4619fc363cc97775b682336587202773
facestatue <facestatued@gmail.com>
2026-01-15 21:31:08 -0500

    Delete js directory

--- js/01_var.js
+++
@@ -1,8 +0,0 @@
-document.write("01_var.js is linked")
-// 1. Declare the variable x using var and assign it the value 5
-
-// 2.  Use a console.log statement to log the value of x to the console.  Refresh the page, then Paste a screenshot of your page and a screenshot of your console into your lab document.
-
-// 3.  Redeclare x using var and assign it the value 2 
-
-// 4.  Use a console.log statement to log the value of x to the console.  Refresh the page, then Paste a screenshot of your page and a screenshot of your console into your lab document.
--- js/02_let.js
+++
@@ -1,8 +0,0 @@
-document.write("02_let.js is linked")
-// 1. Declare the variable y using let and assign it the value 4
-
-// 2.  Use a console.log statement to log the value of y to the console.  Refresh the page, then Paste a screenshot of your page and a screenshot of your console into your lab document.
-
-// 3.  Redeclare y using let and assign it the value 2.  Refresh the page, then paste a screenshot of your page and a screenshot of your console into your lab document. 
-
-// 4.  Delete the "let" you typed in the previous line, refresh your page, then use a console.log statement to log the value of y to the console.  Refresh the page, then Paste a screenshot of your page and a screenshot of your console into your lab document.
